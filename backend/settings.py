120c30c64a4b38480f6152a9378b3caae7796bcd
Sergey Mazilin <mazilin.sergey@yandex.ru>
2025-03-16 13:11:04 +0400

    исправил настройки БД

--- backend/settings.py
+++ backend/settings.py
@@ -71,7 +71,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
