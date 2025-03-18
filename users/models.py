94402ebbf4076d687954ad67f605c20f38c0aebc
Sergey Mazilin <mazilin.sergey@yandex.ru>
2025-03-18 17:47:05 +0400

    Добавил возможность оставлять поля имени и фамилии пустыми в модели TgUser

--- users/models.py
+++ users/models.py
@@ -17,10 +17,14 @@ class TgUser(models.Model):
     )
     first_name = models.CharField(
         max_length=255,
+        blank=True,
+        null=True,
         verbose_name='Имя'
     )
     last_name = models.CharField(
         max_length=255,
+        blank=True,
+        null=True,
         verbose_name='Фамилия'
     )
     birth_date = models.DateField(
